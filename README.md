b5cda8fbf323fc56a0f533f7ab76d706eb7574e0
tanghongying <1483515764@qq.com>
2018-05-16 00:21:47 +0800

    有点困先开个头睡觉

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Express-Node-MongoDB
 学习express和node时的随手
+
+````
+npm start 启动项目
+或者nodemon start
+````
---
+++ db.js
@@ -0,0 +1,17 @@
+// console.log('我是操作数据库的文件！')
+/*
+ *mongoose是一个node.js链接mongodb的orm
+ *orm对象模型关系映射，把一个对象映射到数据库中的表（集合）上
+ *在mongoose中会自动在数据库空生成对象的复数形式对位对象的存储表（集合）
+ */
+const mongoose = require('mongoose'); //引入mongoose插件
+mongoose.Promise = global.Promise; //在node.js中使用内置的promise对象
+const Schema = mongoose.Schema; //Schema模块架构
+mongoose.connect('mongodb://localhost/super_shop', {
+	useMongoClient: true
+});
+
+module.exports = {
+	mongoose: mongoose,
+	Schema: Schema
+}
